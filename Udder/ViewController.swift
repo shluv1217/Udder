832361acd8498115031d19a50e857d6c2de924a6
davidshin <hkluv1009@gmail.com>
2020-09-10 20:16:32 +0900

    Adding didlayoutsubview

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -23,6 +23,10 @@ class ViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         
     }
+    
+    override func viewDidLayoutSubviews() {
+        
+    }
 
 
 }
